5b6f9ff0bb6094184562210ad95c4cad21767639
Gabriel <gabriel20091906@gmail.com>
2021-12-30 11:36:39 -0300

    terceira versao

--- main.js
+++ main.js
@@ -18,10 +18,11 @@ for (const link of links) {
 }
 
 /* mudar o header da pagina quando der scroll */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+
   if (window.scrollY >= navHeight) {
     // scroll é maior que a altura do header
     header.classList.add('scroll')
@@ -29,4 +30,51 @@ window.addEventListener('scroll', function () {
     //scroll é menor que a altura do header
     header.classList.remove('scroll')
   }
+}
+
+/* Testimonials Carousel slider Swiper */
+const swiper = new Swiper('.swiper', {
+  slidesPerview: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* ScrollReveal: Mostrar elementos quando der scroll na pagina */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+ScrollReveal().reveal(
+  `#home .image, #home .text,
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimonals,
+#contact .text, #contact .links,
+footer .brand, footer .social
+`,
+  { interval: 100 }
+)
+
+/* botao voltar para o topo */
+
+function backToTop() {
+  const backTopTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backTopTopButton.classList.add('show')
+  } else {
+    backTopTopButton.classList.remove('show')
+  }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
 })
